88ac0fe46ae88db02a77ff085591a8158100287f
Noman Ali <nomi.rana2020@gmail.com>
2023-05-31 13:01:35 +0500

    More Color Added

--- lib/consts/colors.dart
+++ lib/consts/colors.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 
 const bgColor = Color(0xff1f212c);
 const whiteColor = Color(0xffFFFFFF);
+const white54Color= Color(0x8AFFFFFF);
 const sliderColor = Color(0xff7E70FF);
+const redColor = Color( 0xFFF44336);
+const blueColor = Color(0xFF2196F3);
 const bgDarkColor = Color(0xff070B11);
 const buttonColor = Color.fromRGBO(225, 100, 143, 1.0);
-const splashColor=  Color.fromRGBO(24, 27, 44, 1.0);
+const splashColor =  Color.fromRGBO(24, 27, 44, 1.0);
